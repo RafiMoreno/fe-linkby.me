afd7eeeca0b1e51514ce2ecf15048ec49ef582fc
RafiMoreno <ahmadarrafi.m@gmail.com>
2023-11-02 17:07:45 +0700

    project config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  alias: {
+    '@': resolve(__dirname, "/"),
+
+  },
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/css/main.css'],
 })
---
+++ pages/sign-up.vue
@@ -0,0 +1,3 @@
+<template>
+    <div><h1>LinkByMe</h1></div>
+</template>
