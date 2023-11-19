1b7138b38b19a91e788555fd154ceadcbbc1cfde
RafiMoreno <ahmadarrafi.m@gmail.com>
2023-11-20 00:22:09 +0700

    feat: created static edit color

---
+++ components/ColorEditor.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+        <div class="fixed flex flex-col px-6 py-3 bg-white text-black rounded-[30px] top-1/2 left-1/2 gap-3 translate-y-[-50%] translate-x-[-50%] w-[90%] md:w-[80%] lg:w-[60%]">
+            <Icon @click="$emit('closeEditor')" icon="mingcute:close-fill" color=#A44646 width="24" class="ml-auto" />
+            <p class="text-center font-bold text-xl"> Pick your colours </p>
+            <div class="flex flex-col gap-1">
+                <p class="font-normal text-base">Primary Color</p>
+                <div class="flex flex-row border border-[#B2B2B2] rounded-2xl p-4 align-middle gap-4 cursor-pointer">
+                    <div class="bg-[#A44646] border border-[#B2B2B2] rounded w-[32px] h-[32px]" />
+                    <p class="self-center">#A44646</p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-1">
+                <p class="font-normal text-base">Secondary Color</p>
+                <div class="flex flex-row border border-[#B2B2B2] rounded-2xl p-4 align-middle gap-4 cursor-pointer">
+                    <div class="bg-[#FFFFFF] border border-[#B2B2B2] rounded w-[32px] h-[32px]" />
+                    <p class="self-center">#FFFFFF</p>
+                </div>
+            </div>
+            <Button class="rounded-2xl font-bold text-xl">Submit</Button>
+        </div>
+</template>
--- pages/[username]/edit.vue
+++ pages/[username]/edit.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+    import { Icon } from '@iconify/vue'
+
     const route = useRoute();
     var username: string | string[] = route.params.username;
+    var isColorEditorActive = useState<Boolean>('isColorEditorActive', () => false )
     const data = reactive({
         'displayName': `Edit Page ${username}`,
         'user':{
@@ -59,28 +62,40 @@
 
 
 <template>
-    <div :style="pageStyle" class="h-screen overflow-scroll">
-        <div class="flex flex-col pt-12 pb-3 px-[12px] max-w-[700px] items-center gap-[12px] mx-auto">
-            <!-- <Icon icon="mdi:pencil" /> -->
-            <NuxtImg 
-                v-if="data.displayPicture != undefined && data.displayPicture!=''" 
-                class="object-cover w-[150px] rounded-[50%] aspect-square" 
-                :src="data.displayPicture"
-                />
-            <b class="text-2xl">{{ data.displayName }}</b>
-            <p>{{ data.description }}</p>
-            <div class="h-[25px]"/>
-            <LinkBox 
-                variant="edit" 
-                :style="linkBoxStyle" 
-                v-for="link in data.links" 
-                v-bind:key="link.id" 
-                :url="link.url" 
-                :image-url="link.imageUrl"
-                :handle-edit-click="() => handleEditClick(link)"
-                >
-                {{link.linkTitle}}
-            </LinkBox>
-        </div>
+    
+    <div :style="pageStyle" class="relative h-screen overflow-scroll">
+        <div v-if="isColorEditorActive" class="fixed flex bg-[#000000] h-screen w-full opacity-60 justify-center"/> 
+        <ColorEditor v-if="isColorEditorActive" @close-editor="isColorEditorActive = !isColorEditorActive"/>
+            <div class="flex flex-row p-2">
+                <div>
+                    <Icon @click="isColorEditorActive = !isColorEditorActive" icon="material-symbols:palette-outline" color=pageStyle width="32" class="" />
+                </div>
+                <div class="ml-auto">
+                    <Icon v-on:click="navigateTo(``)" icon="ic:outline-done" color=pageStyle width="32" class="" />
+                </div>
+            </div>
+            <div class="flex flex-col pt-12 pb-3 px-[12px] max-w-[700px] items-center gap-[12px] mx-auto">
+                <!-- <Icon icon="mdi:pencil" /> -->
+                <NuxtImg 
+                    v-if="data.displayPicture != undefined && data.displayPicture!=''" 
+                    class="object-cover w-[150px] rounded-[50%] aspect-square" 
+                    :src="data.displayPicture"
+                    />
+                <b class="text-2xl">{{ data.displayName }}</b>
+                <p>{{ data.description }}</p>
+                <div class="h-[25px]"/>
+                <LinkBox 
+                    variant="edit" 
+                    :style="linkBoxStyle" 
+                    v-for="link in data.links" 
+                    v-bind:key="link.id" 
+                    :url="link.url" 
+                    :image-url="link.imageUrl"
+                    :handle-edit-click="() => handleEditClick(link)"
+                    >
+                    {{link.linkTitle}}
+                </LinkBox>
+            </div>
     </div>
+
 </template>
