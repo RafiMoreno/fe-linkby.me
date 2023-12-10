f5483acf60b8718d1b6ff4918cfbe73b15d04e8a
RafiMoreno <ahmadarrafi.m@gmail.com>
2023-12-10 21:36:45 +0700

    feat: change screen overflow styling

--- components/ProfileEditor.vue
+++ components/ProfileEditor.vue
@@ -43,7 +43,7 @@ const handleSubmit = async () => {
 
 <template>
   <div
-    class="fixed flex flex-col z-20 px-3 py-6 bg-white text-black rounded-[30px] top-1/2 left-1/2 gap-3 translate-y-[-50%] translate-x-[-50%] w-[90%] md:w-[50%] lg:w-[30%]"
+    class="fixed flex flex-col z-20 px-3 py-6 bg-white text-black rounded-[30px] top-1/2 left-1/2 gap-3 translate-y-[-60%] translate-x-[-50%] w-[90%] md:w-[50%] lg:w-[30%]"
   >
     <Icon
       @click="$emit('closeEditor')"
